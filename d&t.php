f54f097f83b01ada5b4ffbe821ee1c166268e793
Mzeewolf <50430475+Mzeewolf@users.noreply.github.com>
2019-07-04 14:08:15 -0700

    Create d&t.php

---
+++ d&t.php
@@ -0,0 +1,17 @@
+<?php
+//Date and Time in PHP created by Mackenzie Smith
+//$date_du_jour = date ("d-m-Y");
+//$heure_courante = date ("H:i");
+echo 'This day is : '.date ("d-m-Y").' And it is : '.date ("H:i:s").'<br>';
+echo 'This day is : '.date ("d-M-Y").' And it is : '.date ("h:i:s").'<br>';
+echo 'This day is : '.date ("d-n-Y").' And it is : '.date ("g:i:s").'<br>';
+echo 'This day is : '.date ("D-m-Y").' And it is : '.date ("H:i:s").'<br>';
+echo 'This day is : '.date ("D-M-Y").' And it is : '.date ("h:i:s").'<br>';
+echo 'This day is : '.date ("D-n-Y").' And it is : '.date ("g:i:s").'<br>';
+echo 'This day is : '.date ("j-m-Y").' And it is : '.date ("H:i:s").'<br>';
+echo 'This day is : '.date ("j-M-Y").' And it is : '.date ("h:i:s").'<br>';
+echo 'This day is : '.date ("j-n-Y").' And it is : '.date ("g:i:s").'<br>';
+echo 'This day is : '.date ("I-m-Y").' And it is : '.date ("H:i:s").'<br>';
+echo 'This day is : '.date ("I-M-Y").' And it is : '.date ("h:i:s").'<br>';
+echo 'This day is : '.date ("I-n-Y").' And it is : '.date ("g:i:s").'<br>';
+?>
